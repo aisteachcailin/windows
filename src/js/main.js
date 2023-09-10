2a26d81050a83827c97b177bfac1c311585e29ef
Anastasia <73635691+ganyushinaanastasia@users.noreply.github.com>
2023-09-10 15:07:40 +0300

    fix menu

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,12 @@ Fancybox.bind("[data-fancybox]", {
     document.querySelector('.header__menu').addEventListener("click", () => {
       document.querySelector('.menu').classList.toggle('open');
     })
+    window.addEventListener('click', e => { // при клике в любом месте окна браузера
+      const target = e.target // находим элемент, на котором был клик
+      if (!target.closest('.menu') && !target.closest('.header__menu')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
+        document.querySelector('.menu').classList.remove('open') // то закрываем окно навигации, удаляя активный класс
+      }
+    })
   }
 
   dropdown();
